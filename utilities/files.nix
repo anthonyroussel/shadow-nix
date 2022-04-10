d9eef9c8670630fdd649e105f64c77b2b4c0f2c4
Anthony Roussel <anthony.roussel17@gmail.com>
2022-04-11 00:48:13 +0200

    Add Git commit revision to fetchGit to ensure pure mode evaluation

--- utilities/files.nix
+++ utilities/files.nix
@@ -5,5 +5,6 @@ rec {
     {
       url = "https://github.com/NicolasGuilloux/blade-shadow-beta";
       ref = "master";
+      rev = "a58bdba241262723431f65e736e4b6dab1f75454";
     } + "/resources/drirc";
 }
