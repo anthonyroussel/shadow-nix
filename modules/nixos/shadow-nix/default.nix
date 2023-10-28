3314cc20615234cc80f9fbad7b046bbfa801be54
Anthony Roussel <anthony@roussel.dev>
2023-10-29 00:23:19 +0200

    feat: migrate to flake

--- modules/nixos/shadow-nix/default.nix
+++ modules/nixos/shadow-nix/default.nix
@@ -5,7 +5,8 @@ let
   cfg = config.programs.shadow-client;
 
   # Declare the package with the appropriate configuration
-  shadow-package = channel: pkgs.callPackage ../default.nix {
+  # shadow-package = channel: pkgs.callPackage ../default.nix {
+  shadow-package = channel: pkgs.callPackage ../../../packages/shadow-nix/default.nix {
     channel = channel;
     enableDiagnostics = cfg.enableDiagnostics;
     enableDesktopLauncher = cfg.enableDesktopLauncher;
@@ -13,7 +14,8 @@ let
 
 in {
   # Import the configuration
-  imports = [ ../config.nix ../x-session ../systemd-session ];
+  #  ./x-session ./systemd-session
+  imports = [ ../../config.nix ];
 
   # By default, if you import this file, the Shadow app will be installed
   programs.shadow-client.enable = lib.mkDefault true;
@@ -28,7 +30,7 @@ in {
     ] ++ lib.forEach cfg.extraChannels shadow-package;
 
     # Add GPU fixes
-    etc.drirc.source = lib.mkIf (cfg.enableGpuFix) ./.drirc;
+    etc.drirc.source = lib.mkIf (cfg.enableGpuFix) ../.drirc;
 
     # Force VA Driver
     variables.LIBVA_DRIVER_NAME = lib.mkIf (cfg.forceDriver != null) [ cfg.forceDriver ];
